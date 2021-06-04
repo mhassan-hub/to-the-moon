da0453682bcba9224275e9896250476a23daa6e9
Omar Irfan <omar.irfan18@gmail.com>
2021-06-04 23:18:29 +0000

    initial front-end setup

--- backend-rails/app/controllers/sessions_controller.rb
+++ backend-rails/app/controllers/sessions_controller.rb
@@ -1,23 +1,38 @@
 class SessionsController < ApplicationController
-  def new
-  end
+  # def new
+  # end
+
+  # def create
+  #   user = User.find_by_email(params[:email])
+  #   # If the user exists AND the password entered is correct.
+  #   if user && user.authenticate(params[:password])
+  #     # Save the user id inside the browser cookie. This is how we keep the user 
+  #     # logged in when they navigate around our website.
+  #     session[:user_id] = user.id
+  #     redirect_to '/'
+  #   else
+  #   # If user's login doesn't work, send them back to the login form.
+  #     redirect_to '/login'
+  #   end
+  # end
+
+  # def destroy
+  #   session[:user_id] = nil
+  #   redirect_to '/login'
+  # end
 
   def create
-    user = User.find_by_email(params[:email])
-    # If the user exists AND the password entered is correct.
-    if user && user.authenticate(params[:password])
-      # Save the user id inside the browser cookie. This is how we keep the user 
-      # logged in when they navigate around our website.
-      session[:user_id] = user.id
-      redirect_to '/'
-    else
-    # If user's login doesn't work, send them back to the login form.
-      redirect_to '/login'
-    end
-  end
+    user = User.find_by(email: params["user"]["email"]).try(:authenticate, params["user"]["password"])
 
-  def destroy
-    session[:user_id] = nil
-    redirect_to '/login'
+    if user
+        session[:user_id] = user.id
+        render json: {
+            status: :created,
+            logged_in: true,
+            user: user
+        } 
+    else
+        render json: { status: 401 }
+    end
   end
 end
--- backend-rails/app/models/user.rb
+++ backend-rails/app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates_presence_of :email
+  validates_uniqueness_of :email
 end
