72ea5ece7cfe9d5aaf44eac96afaf35c42538817
Omar Irfan <omar.irfan18@gmail.com>
2021-06-05 20:10:19 +0000

    added shooting functionality

--- client/src/components/scenes/Menu.js
+++ client/src/components/scenes/Menu.js
@@ -1,19 +1,62 @@
 import Phaser from "phaser";
 
+class Laser extends Phaser.Physics.Arcade.Sprite {
+  constructor(scene, x, y) {
+    super(scene, x, y, 'laser');
+  }
 
+  fire(x,y) {
+    this.body.reset(x, y)
+    this.setActive(true)
+    this.setVisible(true)
+    this.setVelocityY(-900)
+  }
 
-export default class Menu extends Phaser.Scene {
+  preUpdate(time, delta) {
+    super.preUpdate(time, delta);
+    if(this.y <= 0) {
+      this.setActive(false)
+    this.setVisible(false)
+    }
+  }
+}
+
+class LaserGroup extends Phaser.Physics.Arcade.Group {
+  constructor(scene) {
+    super(scene.physics.world, scene);
+
+    this.createMultiple({
+      classType: Laser,
+      frameQuantity: 30,
+      active: false,
+      visible: false,
+      key: 'laser',
+      setScale: {x:0.2 ,y:0.4}
+    })
+  } 
+  fireLaser(x,y) {
+    const laser = this.getFirstDead(false);
+    if (laser) {
+      laser.fire(x, y);
+    }
+  }
+}
+
+export default class Menu extends Phaser.Scene {  
   constructor() {
     super("Menu");
+
+    // this.laserGroup;
   }
 
   
-
+ 
   preload() {
     // this.load.audio("audioSound", "assets/Demon.mp3");
     this.load.image("asteroid", "assets/Asteroid.png")
     this.load.image("ship", "assets/fighter.png");
     this.load.image("background", "assets/starfield.png")
+    this.load.image("laser","assets/laser.png")
   }
   create() {
     this.add.image(400, 300, 'background')
@@ -21,6 +64,8 @@ export default class Menu extends Phaser.Scene {
     this.player = this.physics.add.sprite(500, 700, 'ship');
     // this.asteroid = this.physics.add.sprite(Math.floor(Math.random() * 800) ,0, "asteroid")
     // this.physics.add.existing(this.asteroid, true)
+
+    this.laserGroup = new LaserGroup(this);
     
     this.player.setCollideWorldBounds(true);
 
@@ -52,6 +97,8 @@ export default class Menu extends Phaser.Scene {
     // this.music.play();
 
     this.cursors = this.input.keyboard.createCursorKeys()
+
+    this.space = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
 
     setObjVelocity(asteroids)
 
@@ -100,9 +147,14 @@ export default class Menu extends Phaser.Scene {
       
 
     }
+    if (this.space.isDown) {
+      this.shootLaser();
+    }
   }
 
-  
+  shootLaser() {
+    this.laserGroup.fireLaser(this.player.x, this.player.y - 20);
+  }
 
 
 }
