f7752d209fd976b27f21348856a98669f683f263
Omar Irfan <omar.irfan18@gmail.com>
2021-06-05 18:45:57 +0000

    asteroid functionality

--- client/src/components/scenes/Menu.js
+++ client/src/components/scenes/Menu.js
@@ -10,7 +10,7 @@ export default class Menu extends Phaser.Scene {
   
 
   preload() {
-    // this.load.audio("audioSound", "assets/Demon Slayer Kimetsu no Yaiba.mp3");
+    // this.load.audio("audioSound", "assets/Demon.mp3");
     this.load.image("asteroid", "assets/Asteroid.png")
     this.load.image("ship", "assets/fighter.png");
     this.load.image("background", "assets/starfield.png")
@@ -18,50 +18,91 @@ export default class Menu extends Phaser.Scene {
   create() {
     this.add.image(400, 300, 'background')
     this.background = this.add.tileSprite(0, 0, 0, 0, 'background').setOrigin(0);
-    this.asteroid = this.physics.add.sprite(300,600, "asteroid")
-    this.physics.add.existing(this.asteroid, true)
-    this.player = this.physics.add.sprite(100, 450, 'ship');
+    this.player = this.physics.add.sprite(500, 700, 'ship');
+    // this.asteroid = this.physics.add.sprite(Math.floor(Math.random() * 800) ,0, "asteroid")
+    // this.physics.add.existing(this.asteroid, true)
+    
     this.player.setCollideWorldBounds(true);
-  
-   
 
-    this.physics.add.collider(this.player, this.asteroid)
+    const asteroids = this.physics.add.group({
+      key: 'asteroid',
+      repeat: 3,
+      setXY: {
+        x:(Math.floor(Math.random() * 800)), y: 0, stepX: Phaser.Math.Between(10, 750), stepY: Phaser.Math.Between(15, 300)
+      }
+    })
+
+    function bounce (player, asteroid) {
+      asteroid.destroy()
+      
+      setTimeout( () => {
+        player.setPosition(500,700)}, 300
+      )
+    }
+    
+
+    this.physics.add.collider(this.player, asteroids)
+    // this.physics.add.collider(this.player, this.asteroids, ()=>{
+    //   bounce(this.player, this.asteroids)
+    // }
+    // )
+    // this.onCollide = true;
 
     // this.music = this.sound.add("audioSound", { volume: 0.1, loop: true});
     // this.music.play();
 
     this.cursors = this.input.keyboard.createCursorKeys()
+
+    setObjVelocity(asteroids)
+
+    function setObjVelocity (asteroids) {
+      asteroids.children.iterate(function(asteroid) {
+          let xVel = Phaser.Math.Between(-100, 100)
+          let yVel = Phaser.Math.Between(150,200)
+          asteroids.setVelocity(xVel,yVel)
+      })
+    }
+
   }
 
   update() {
   
+    console.log(this.asteroids)
+
     this.background.tilePositionY -= 3
-   
+    // this.asteroids.y += 2
+    // // this.asteroid.velocity.y -= 3
+    // // 1 in 10 chance for creating an astroid
+    // if (Math.floor(10000 * Math.random()) === 1) {
+    //   // this.asteroid = this.physics.add.sprite((Math.floor(800 * Math.random())), 100, "asteroid")
+    //   const asteroid = this.asteroids.getChildren()
+
+    //   asteroid.setVelocity(400,300)
+    //   asteroid.y += 2
+    // }
 
     /** @type {Phaser.Phyics.Arcade.StaticBody} */
-    const body = this.player.body
 
     if(this.cursors.up.isDown) {
       this.player.y -= 10
-      
-      body.updateFromGameObject()
       
     }
     else if(this.cursors.down.isDown) {
       this.player.y += 10
       
-      body.updateFromGameObject()
     }
     else if(this.cursors.left.isDown) {
       this.player.x -= 10
-      
-      body.updateFromGameObject()
+
     }
     else if(this.cursors.right.isDown) {
       this.player.x += 10
       
-      body.updateFromGameObject()
+
     }
   }
+
+  
+
 
 }
