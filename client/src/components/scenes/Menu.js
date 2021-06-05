67c87af596da50358c9d614e8a35a6a82820c065
Mohamed Hassan <mhass405@gmail.com>
2021-06-05 18:14:30 -0400

    Add switch scene to win functionality based on distance traveled

--- client/src/components/scenes/Menu.js
+++ client/src/components/scenes/Menu.js
@@ -1,75 +1,75 @@
 import Phaser from "phaser";
 
-
-
+let timedEvent;
+let text;
 export default class Menu extends Phaser.Scene {
   constructor() {
     super("Menu");
   }
 
-  
-
   preload() {
     // this.load.audio("audioSound", "assets/Demon.mp3");
-    this.load.image("asteroid", "assets/Asteroid.png")
+    this.load.image("asteroid", "assets/Asteroid.png");
     this.load.image("ship", "assets/fighter.png");
-    this.load.image("background", "assets/starfield.png")
+    this.load.image("background", "assets/starfield.png");
   }
+
   create() {
-    this.add.image(400, 300, 'background')
-    this.background = this.add.tileSprite(0, 0, 0, 0, 'background').setOrigin(0);
-    this.player = this.physics.add.sprite(500, 700, 'ship');
-    // this.asteroid = this.physics.add.sprite(Math.floor(Math.random() * 800) ,0, "asteroid")
-    // this.physics.add.existing(this.asteroid, true)
-    
+    this.add.image(400, 300, "background");
+    this.background = this.add
+      .tileSprite(0, 0, 0, 0, "background")
+      .setOrigin(0);
+
+    this.player = this.physics.add.sprite(500, 700, "ship");
+
+    // timedEvent = this.time.delayedCall(
+    //   5000,
+    //   () => {
+    //     this.scene.start("Win");
+    //     this.scene.stop("Menu");
+    //   },
+    //   this
+    // );
+
     this.player.setCollideWorldBounds(true);
 
     const asteroids = this.physics.add.group({
-      key: 'asteroid',
+      key: "asteroid",
       repeat: 3,
       setXY: {
-        x:(Math.floor(Math.random() * 800)), y: 0, stepX: Phaser.Math.Between(10, 750), stepY: Phaser.Math.Between(15, 300)
-      }
-    })
+        x: Math.floor(Math.random() * 800),
+        y: 0,
+        stepX: Phaser.Math.Between(10, 750),
+        stepY: Phaser.Math.Between(15, 300),
+      },
+    });
 
-    function bounce (player, asteroid) {
-      asteroid.destroy()
-      
-      setTimeout( () => {
-        player.setPosition(500,700)}, 300
-      )
-    }
-    
-
-    this.physics.add.collider(this.player, asteroids)
-    // this.physics.add.collider(this.player, this.asteroids, ()=>{
-    //   bounce(this.player, this.asteroids)
-    // }
-    // )
-    // this.onCollide = true;
+    this.physics.add.collider(this.player, asteroids);
 
     // this.music = this.sound.add("audioSound", { volume: 0.1, loop: true});
     // this.music.play();
 
-    this.cursors = this.input.keyboard.createCursorKeys()
+    this.cursors = this.input.keyboard.createCursorKeys();
 
-    setObjVelocity(asteroids)
+    setObjVelocity(asteroids);
 
-    function setObjVelocity (asteroids) {
-      asteroids.children.iterate(function(asteroid) {
-          let xVel = Phaser.Math.Between(-100, 100)
-          let yVel = Phaser.Math.Between(150,200)
-          asteroids.setVelocity(xVel,yVel)
-      })
+    function setObjVelocity(asteroids) {
+      asteroids.children.iterate(function (asteroid) {
+        let xVel = Phaser.Math.Between(-100, 100);
+        let yVel = Phaser.Math.Between(150, 200);
+        asteroids.setVelocity(xVel, yVel);
+      });
     }
-
   }
 
   update() {
-  
-    console.log(this.asteroids)
+    this.background.tilePositionY -= 3;
 
-    this.background.tilePositionY -= 3
+    if (this.background.tilePositionY < -1000) {
+      this.scene.start("Win");
+      this.scene.stop("Menu");
+    }
+    // console.log(this.background.tilePositionY);
     // this.asteroids.y += 2
     // // this.asteroid.velocity.y -= 3
     // // 1 in 10 chance for creating an astroid
@@ -83,26 +83,14 @@ export default class Menu extends Phaser.Scene {
 
     /** @type {Phaser.Phyics.Arcade.StaticBody} */
 
-    if(this.cursors.up.isDown) {
-      this.player.y -= 10
-      
-    }
-    else if(this.cursors.down.isDown) {
-      this.player.y += 10
-      
-    }
-    else if(this.cursors.left.isDown) {
-      this.player.x -= 10
-
-    }
-    else if(this.cursors.right.isDown) {
-      this.player.x += 10
-      
-
+    if (this.cursors.up.isDown) {
+      this.player.y -= 10;
+    } else if (this.cursors.down.isDown) {
+      this.player.y += 10;
+    } else if (this.cursors.left.isDown) {
+      this.player.x -= 10;
+    } else if (this.cursors.right.isDown) {
+      this.player.x += 10;
     }
   }
-
-  
-
-
 }
