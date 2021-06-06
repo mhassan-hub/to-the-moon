a1b1ecd62aa811bdb6baed4bb30ce9eff4e5c3be
Mohamed Hassan <mhass405@gmail.com>
2021-06-06 12:25:30 -0400

    Add landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,54 @@
-import Game from './components/game'
+import Game from "./components/game";
 import "./App.css";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Button from "./components/Button";
 
 function App() {
-  useEffect(() => {
-    axios.get("http://localhost:3000/users").then((result) => {console.log(result) 
-    });
-  }, []);
+  const [state, setState] = useState(false);
 
+  // useEffect(() => {
+  //   axios.get("http://localhost:3000/users").then((result) => {
+  //     console.log(result);
+  //   });
+  // }, []);
+
+  function startGame() {
+    return setState(true);
+  }
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src="https://i.pinimg.com/originals/f6/20/a1/f620a179ca1acfd15a0754701677fb58.jpg" className="background" alt="background"></img>
-    //     <img src="https://www.clipartmax.com/png/middle/6-63877_ship-anchor-free-a-cartoon-moon-rocket-cartoon-rocket-ship.png" className="App-logo" alt="rocketship"/>
-    //     <h1>
-    //       To The Moon!
-    //     </h1>
-    //     <h3>Description</h3>
+    <div className="App">
+      {state ? (
+        <Game />
+      ) : (
+        <header className="App-header">
+          {/* <img
+          src="https://i.pinimg.com/originals/f6/20/a1/f620a179ca1acfd15a0754701677fb58.jpg"
+          className="background"
+          alt="background"
+        ></img> */}
+          <img
+            src="https://www.clipartmax.com/png/middle/6-63877_ship-anchor-free-a-cartoon-moon-rocket-cartoon-rocket-ship.png"
+            className="App-logo"
+            alt="rocketship"
+          />
+          <h1>To The Moon!</h1>
+          <h3>Description</h3>
 
-    //   <div className="buttonContainer">
-    //   <Button onClick={() => {
-    //     console.log("start")
-    //   }}>
-    //     About us</Button>
-    //   <Button>Create Game</Button>
-    //   </div>
-    //   </header>
-      <Game />
-    // </div>
-
+          <div className="buttonContainer">
+            <Button
+              onClick={() => {
+                console.log("About us");
+              }}
+            >
+              About us
+            </Button>
+            <Button onClick={() => startGame()}>Create Game</Button>
+            {/* {state ? <Game /> : console.log(false)} */}
+          </div>
+        </header>
+      )}
+    </div>
   );
 }
 
