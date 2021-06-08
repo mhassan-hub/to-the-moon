95e92a658ea42a45d65fcf9c2f6837b92bd07c03
Grigor-ship-it <gb795@hotmail.com>
2021-06-08 03:00:03 +0000

    add front end login logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,39 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, {useEffect, useState} from "react";
 import Game from "./components/Game";
 import Home from "./components/Home";
 import About from "./components/About";
 import "./App.css";
 
 function App() {
-  // useEffect(() => {
-  //   axios.get("http://localhost:3000/users").then((result) => {
-  //     console.log(result);
-  //   });
-  // }, []);
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/users", 
+    {withCredentials: true})
+    .then(response => {
+      if (response.data.logged_in) {
+        this.handleLogin(response)
+      } else {
+        this.handleLogout()
+      }
+    })
+    .catch(error => console.log('api errors:', error))
+  }, []);
+
+  handleLogin = (data) => {
+    this.setState({
+      isLoggedIn: true,
+      user: data.user
+    })
+  }
+  handleLogout = () => {
+    this.setState({
+    isLoggedIn: false,
+    user: {}
+    })
+  }
 
   return (
     <div className="App">
