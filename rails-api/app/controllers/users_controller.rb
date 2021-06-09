258b5b23e01fef518cfd0eb96c73df452513a339
Omar Irfan <omar.irfan18@gmail.com>
2021-06-09 21:42:49 +0000

    made axios get and post requests for leader

--- rails-api/app/controllers/users_controller.rb
+++ rails-api/app/controllers/users_controller.rb
@@ -42,9 +42,26 @@ def show
           }
           end
     end
+    def submit_score
+        @user = User.find(params[:id])
+
+        # if params[:high_score] > @user.high_score
+        puts params[:high_score]
+        @user.update(high_score: params[:high_score], email: @user.email, password: @user.password_digest,  username: @user.username)
+        render json: {
+            status: 200
+        }
+        # end
+    end
+
+    def show_scores
+        high_scores = User.all.select(:username, :high_score).order(high_score: :desc).limit(5)
+        render json: {high_scores: high_scores.as_json(:except => [:id])}
+    end
 private
     
    def user_params
        params.require(:user).permit(:username, :email, :password, :password_confirmation)
    end
+
 end
