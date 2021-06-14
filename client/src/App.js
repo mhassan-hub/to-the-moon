a738ea244dd897f959bdc32d9666b9f64354b6a1
Mohamed Hassan <mhass405@gmail.com>
2021-06-14 16:21:09 -0400

    style change

--- client/src/App.js
+++ client/src/App.js
@@ -1,72 +1,77 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import React from "react";
 import Game from "./components/Game";
-import Login from "./components/Login"
+import Login from "./components/Login";
 import Home from "./components/Home";
-import Register from "./components/Register"
+import Register from "./components/Register";
 import About from "./components/About";
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import Leaderboard from "./components/Leaderboard"
-import Button from '@material-ui/core/Button';
+import Leaderboard from "./components/Leaderboard";
+import Button from "@material-ui/core/Button";
 
 const theme = createMuiTheme({
   palette: {
     background: {
-      default: '#8561c5'
+      default: "#8561c5",
     },
     primary: {
-      light: '#8561c5',
-      main: '#673ab7',
-      dark: '#482880',
-      contrastText: '#fff',
+      light: "#8561c5",
+      main: "#673ab7",
+      dark: "#482880",
+      contrastText: "#fff",
     },
     secondary: {
-      light: '#ffcf33',
-      main: '#ffc400',
-      dark: '#b28900',
-      contrastText: '#fff',
+      light: "#ffcf33",
+      main: "#ffc400",
+      dark: "#b28900",
+      contrastText: "#fff",
     },
   },
   props: {
     StyledTableCell: {
       varient: "contained",
-      color: "primary"
-    }
-  }
+      color: "primary",
+    },
+  },
 });
 
 function App() {
-
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline/>
-    <div className="App">
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-      <Router>
-        <Switch>
-          <Route path="/game">
-            <Game />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/leaderboard">
-            <Leaderboard />
-          </Route>
-          <Route path="/register">
-            <Register />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+      <CssBaseline />
+      <div className="App">
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+        <Router>
+          <Switch>
+            <Route path="/game">
+              <Game />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/leaderboard">
+              <Leaderboard />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
     </ThemeProvider>
   );
 }
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,43 +1,72 @@
-import react from 'react'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import IconButton from '@material-ui/core/IconButton'
-import Typography from '@material-ui/core/Typography'
-import Button from '@material-ui/core/Button'
-import Link from '@material-ui/core/Link'
-import { useState, useEffect } from "react";
+import React, { useState } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Link from "@material-ui/core/Link";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { HomeIcon } from "@material-ui/icons/Home";
+import "./NavBar.css";
 
-export default function NavBar() { 
-
-  const [response, setResponse] = useState("");
-
+export default function NavBar() {
   const killSession = () => {
-    sessionStorage.clear()
-    window.location.reload() 
-  }
-  
-  return(<AppBar position="static">
-  <Toolbar>
-    <IconButton edge="start" className="Home" color="inherit" aria-label="home">
-    </IconButton>
-    <Typography href="/" variant="h6">To The Moon</Typography>
-    {sessionStorage.length === 0 &&
-    <div>
-    <Link href="/login">
-    <Button variant="inherit" color="secondary">Log in</Button>
-    </Link>
-    <Link href="/register">
-    <Button variant="inherit" color="secondary">Register</Button>            
-    </Link>
-    </div>}
-    {sessionStorage.length > 0 &&         
-    <Button onClick={killSession}>Log Out</Button>}
-    <Link href="/about">
-    <Button variant="inherit" color="secondary">About</Button>
-    </Link>
-    <Link href="/leaderboard">
-    <Button variant="inherit" color="secondary">Leaderboard</Button>
-    </Link>
-  </Toolbar>
-</AppBar>)
+    sessionStorage.clear();
+    window.location.reload();
+  };
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <div className="div-navbar-buttons">
+          <div className="div-buttons-left">
+            <Typography href="/" variant="h6">
+              To The Moon
+            </Typography>
+          </div>
+          <div className="div-buttons-right">
+            {sessionStorage.length === 0 && (
+              <div>
+                <Button className="nav-button" href="/">
+                  <HomeIcon />
+                </Button>
+                <Button className="nav-button" href="/login" variant="inherit">
+                  Log in
+                </Button>
+                <Button
+                  className="nav-button"
+                  href="/register"
+                  variant="inherit"
+                >
+                  Register
+                </Button>
+              </div>
+            )}
+            {sessionStorage.length > 0 && (
+              <Button className="nav-button" onClick={killSession}>
+                Log Out
+              </Button>
+            )}
+            <Button
+              className="nav-button"
+              href="/about"
+              variant="inherit"
+              color="secondary"
+            >
+              About
+            </Button>
+            <Button
+              className="nav-button"
+              href="/leaderboard"
+              variant="inherit"
+              color="secondary"
+            >
+              Leaderboard
+            </Button>
+          </div>
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
 }
